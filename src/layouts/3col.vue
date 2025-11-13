6f60810e389be81c4541e540220a6ecf3f36c573
Mark Atherton <mark@insightinternet.co.uk>
2025-11-13 12:14:39 +0000

    added ThreeColLayout with default Cards to <Home />

---
+++ src/layouts/3col.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Card from "../components/Card.vue";
+</script>
+<template lang="">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="bg-base-200 p-4 rounded-box"><Card /></div>
+    <div class="bg-base-200 p-4 rounded-box"><Card /></div>
+    <div class="bg-base-200 p-4 rounded-box"><Card /></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ThreeColLayout",
+};
+</script>
+<style lang=""></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterView } from "vue-router";
 import HeroSlideTransition from "../components/HeroSlideTransition.vue";
+import ThreeColLayout from "../layouts/3col.vue";
 import Carousel from "../components/Carousel.vue";
 </script>
 <template>
   <div>
     <RouterView />
     <HeroSlideTransition />
+    <ThreeColLayout />
     <Carousel />
   </div>
 </template>
