842a8ae20635188e9d9870339d3077d03866c0cb
Mark Atherton <mark@insightinternet.co.uk>
2025-11-13 14:34:02 +0000

    added intro text to homepage

---
+++ src/layouts/2col.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+    <div class="bg-base-200 p-4 rounded-box">
+      High Exchange is a business to business solution that connects local
+      growers with retailers.
+    </div>
+    <div class="bg-base-200 p-4 rounded-box">
+      We pride ourselves in offering a safe space where growers and retailers
+      can exchange the highest quality products directly from the source.
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TwoColLayout",
+};
+</script>
+<style lang=""></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,12 +2,14 @@
 import { RouterView } from "vue-router";
 import HeroSlideTransition from "../components/HeroSlideTransition.vue";
 import ThreeColLayout from "../layouts/3col.vue";
+import TwoColLayout from "../layouts/2col.vue";
 import Carousel from "../components/Carousel.vue";
 </script>
 <template>
   <div>
     <RouterView />
     <HeroSlideTransition />
+    <TwoColLayout />
     <ThreeColLayout />
     <Carousel />
   </div>
