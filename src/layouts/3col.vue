e44ec80472b3565aee35347b9123f817ae54efe6
Mark Atherton <mark@insightinternet.co.uk>
2025-11-17 11:06:37 +0000

    used v-for to loop over the number titles in the titles to display the appropriate number of Card components to display each title in the array

--- src/layouts/3col.vue
+++ src/layouts/3col.vue
@@ -6,30 +6,18 @@ import MobyDick from "../assets/growersFlowers/MobyDick.jpeg";
 import { ref } from "vue";
 
 const titles = ref(["Cinderella 99", "Gorilla Zkittles", "Moby dick"]);
+const images = ref([cinderella99, GorillaZkittles, MobyDick]);
 const currentIndex = ref(0);
 </script>
+
 <template lang="">
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    <div class="bg-base-200 p-4 rounded-box">
-      <Card
-        :imageSrc="cinderella99"
-        :title="titles[currentIndex]"
-        alt="Cinderella 99"
-      />
-    </div>
-    <div class="bg-base-200 p-4 rounded-box">
-      <Card
-        :imageSrc="GorillaZkittles"
-        :title="titles[currentIndex + 1]"
-        alt="Gorilla Zkittles"
-      />
-    </div>
-    <div class="bg-base-200 p-4 rounded-box">
-      <Card
-        :imageSrc="MobyDick"
-        :title="titles[currentIndex + 2]"
-        alt="Moby dick"
-      />
+    <div
+      v-for="(title, index) in titles"
+      :key="index"
+      class="bg-base-200 p-4 rounded-box"
+    >
+      <Card :imageSrc="images[index]" :title="title" :alt="title" />
     </div>
   </div>
 </template>
